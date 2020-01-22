3a6af3f38d435132faa3488014aec1ec7d13c20c
andrew-mikesell-owatc <andrew.mikesell2271@omail.otech.edu>
2020-01-22 13:33:22 -0700

    First commit

--- LocalGit/Form1.cs
+++ LocalGit/Form1.cs
@@ -16,5 +16,10 @@ namespace LocalGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world of Git");
+        }
     }
 }
